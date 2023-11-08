edba4046bd56950ce3e44b430dda78e81bca01d1
nandinho23 <132925522+nandinho23@users.noreply.github.com>
2023-11-08 12:21:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,36 @@
-function tocaSomPom(idElementoAudio){
-  document.querySelector (idElementoAudio).play();
-
- if (element === null) {
+function tocaSom(idElementoAudio){
+  const elemento = document.querySelector(idElementoAudio);
+  if (elemento === null) {
   alert('Elemento não encontrado');
-}
+};
   if (elemento && elemento.localName === 'audio'){
     elemento.play();
   }else {
     alert('Elemento não encontrado');
-  }
+  };
 }
-    const listaDeTeclas = document.querySelectorAll('.tecla');
-
+const listaDeTeclas = document.querySelectorAll('.tecla');
       for(let contador = 0; contador < listaDeTeclas.length;contador++) {
            const tecla = listaDeTeclas[contador];
-           const instrumento = tecla.ClassList[1]
+           const instrumento = tecla.classList[1]
            const idAudio = `#som_${instrumento}`;
            console.log(idAudio);
-          console.log(instrumento);
-
+         
           tecla.onclick = function(){
-              tocaSomPom(idAudio);
-          }
+              tocaSom(idAudio);
+          };
           tecla.onkeydown= function(evento){
             if(evento.code ==='Enter' || evento.code ==='Space'){
             tecla.classList.add('ativa');
           }
+        };
           tecla.onkeyup = function(){
             tecla.classList.remove('ativa');
+          };
+          console.log(instrumento);
+      console.log(contador);
           }
-          console.log(contador)
-          }
-          }
+          
          
 
         
